9e0267d0132e577485a5ae70a0f2c9534cccda8e
Seth Purr <eric.purrington@gmail.com>
2020-05-08 11:05:04 -0700

    fully functioning CLI, just need to write readme and style

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,109 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const allEmployees = [];
 
+const newEmployeeQs = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is the new employee's role?",
+        choices: ["Manager", "Engineer", "Intern"]
+
+    }, {
+        type: "input",
+        name: "employeeName",
+        message: "What is the new employee's name?"
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the new employee's id?"
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the new employee's email?"
+    }
+];
+
+const newManagerQ = [
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the new Manager's office number?"
+    }
+];
+
+const newEngineerQ = [
+    {
+        type: "input",
+        name: "github",
+        message: "What is the new Engineer's GitHub username?"
+    }
+];
+
+const newInternQ = [
+    {
+        type: "input",
+        name: "school",
+        message: "What is the new Intern's school name?"
+    }
+];
+
+const addEmployee = [
+    {
+        type: "list",
+        name: "addEmployee",
+        message: "Would you like to add a new employee?",
+        choices: ["Yes", "No"]
+    }
+]
+
+init();
+
+function init() {
+    inquirer.prompt(addEmployee).then(function (data) {
+        if (data.addEmployee === "Yes") {
+            newEmployee();
+        } else {
+            writeToFile(outputPath, render(allEmployees));
+        }
+    });
+}
+
+function newEmployee() {
+    inquirer.prompt(newEmployeeQs).then(function (data) {
+        if (data.role === "Manager") {
+            // let manager = new Manager(data.employeeName, data.id, data.email);
+            inquirer.prompt(newManagerQ).then(function (officeNumber) {
+                let manager = new Manager(data.employeeName, data.id, data.email, officeNumber.officeNumber);
+                allEmployees.push(manager);
+                init();
+            });
+        }
+        if (data.role === "Engineer") {
+            inquirer.prompt(newEngineerQ).then(function (github) {
+                let engineer = new Engineer(data.employeeName, data.id, data.email, github.github);
+                allEmployees.push(engineer);
+                init();
+            });
+        }
+        if (data.role === "Intern") {
+            inquirer.prompt(newInternQ).then(function (school) {
+                let intern = new Intern(data.employeeName, data.id, data.email, school.school);
+                allEmployees.push(intern);
+                init();
+            });
+        }
+    });
+}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success! Your team.html has been created!");
+    });
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
